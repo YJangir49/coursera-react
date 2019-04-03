a81deadb5c021700cb327255e4081bfa6df157db
Yogender Jangir <201651054@iiitvadodara.ac.in>
2019-04-03 13:01:58 +0530

    State Lifting and Event handlong

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent'
 import './App.css';
+import {DISHES} from './shared/dishes'
 
 
 class App extends Component {
+
+  constructor(props){
+      super(props);
+      this.state = {
+        dishes: DISHES
+      };
+
+  }
   render() {
     return (
       <div className="App">
@@ -13,7 +22,7 @@ class App extends Component {
                 <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
             </div>
         </Navbar>
-        <Menu/>
+        <Menu dishes={this.state.dishes }/>
       </div>
     );
   }
