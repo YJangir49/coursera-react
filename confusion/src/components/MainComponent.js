61cfda63b24504050d758f84c3e9760f8c680072
Yogender Jangir <201651054@iiitvadodara.ac.in>
2019-04-14 05:13:56 +0530

    Aboutus page added Assignment 2

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -9,6 +9,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent'
 import {Switch, Route, Redirect} from 'react-router-dom'
 import Home from './HomeComponent'
+import About from './AboutComponent' 
 import Contact from './ContactComponent' 
 
 
@@ -45,6 +46,7 @@ class Main extends Component {
           <Route path ='/home' component={HomePage}/>
           <Route exact path ='/menu' component={()=><Menu dishes={this.state.dishes}/>}/>
           <Route path='/menu/:dishId' component={DishWithId}/>
+          <Route path='/aboutus' component={()=><About leaders={this.state.leaders}/>}/>
           <Route exact path='/contactus' component={Contact}/>
           <Redirect to='/home'/>
         </Switch>
