fd95a03a861b96ec968e0e94940c3b460e71481f
Yogender Jangir <201651054@iiitvadodara.ac.in>
2019-05-27 03:36:52 +0530

    CSS Transitions/ React animation

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -10,6 +10,8 @@ import About from './AboutComponent'
 import Contact from './ContactComponent' 
 import {connect} from 'react-redux'
 import {postComment, fetchDishes, fetchComments, fetchPromos} from '../redux/ActionCreators'
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
 
 const mapStateToProps = state =>{
   return{
@@ -69,15 +71,19 @@ class Main extends Component {
   }
     return (
       <div className="App">
-        <Header/>`
-        <Switch>
-          <Route path ='/home' component={HomePage}/>
-          <Route exact path ='/menu' component={()=><Menu dishes={this.props.dishes}/>}/>
-          <Route path='/menu/:dishId' component={DishWithId}/>
-          <Route path='/aboutus' component={()=><About leaders={this.props.leaders}/>}/>
-          <Route exact path='/contactus' component={()=><Contact resetFeedbackForm={this.props.resetFeedbackForm}/>}/>
-          <Redirect to='/home'/>
-        </Switch>
+        <Header/>
+        <TransitionGroup>
+            <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+              <Switch>
+                <Route path ='/home' component={HomePage}/>
+                <Route exact path ='/menu' component={()=><Menu dishes={this.props.dishes}/>}/>
+                <Route path='/menu/:dishId' component={DishWithId}/>
+                <Route path='/aboutus' component={()=><About leaders={this.props.leaders}/>}/>
+                <Route exact path='/contactus' component={()=><Contact resetFeedbackForm={this.props.resetFeedbackForm}/>}/>
+                <Redirect to='/home'/>
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
         <Footer/>
       </div>
     );
