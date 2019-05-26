3f213884c789494f801867311a843f81edc8e352
Yogender Jangir <201651054@iiitvadodara.ac.in>
2019-05-27 03:54:13 +0530

    Dish Detail Component/Card FadeTranform/React animation

--- confusion/src/components/DishDetailsComponent.js
+++ confusion/src/components/DishDetailsComponent.js
@@ -4,6 +4,7 @@ import {Link} from 'react-router-dom';
 import {Loading} from './LoadingComponent'
 import {Control, LocalForm, Errors} from 'react-redux-form'
 import {baseUrl} from '../shared/baseUrl'
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const maxLength=(len)=>(val)=>!(val)||(val.length <= len);
 const minLength=(len)=>(val)=> val && (val.length>=len);
@@ -92,13 +93,19 @@ class CommentForm extends Component {
         if(dish!=null)
         {
             return(
-               <Card>
-                 <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
-                    <CardBody>
-                         <CardTitle>{dish.name}</CardTitle>
-                         <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
+                <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                    <Card>
+                        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
+                        <CardBody>
+                             <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </FadeTransform>
              )
         }
         else
